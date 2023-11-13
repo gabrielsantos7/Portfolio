9bb9d468099b8fdaa73763d3d889258a9aed2287
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-11-13 20:49:21 -0300

    finished abput me session

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   new TypeIt(".animated", {
     speed: 200,
-    // strings: ["Front-end", "Back-end", "Full-Stack"],
     loop: true
   })
   .type("Front-end", { delay: 900 })
@@ -10,5 +9,19 @@ document.addEventListener("DOMContentLoaded", () => {
   .delete(8)
   .type("Full-Stack", { delay: 1000 })
   .delete(10)
+  .go();
+
+  new TypeIt(".print", {
+    speed: 200,
+    loop: true
+  })
+  .type("console.log", { delay: 1100 })
+  .delete(11)
+  .type("print", { delay: 1000 })
+  .delete(5)
+  .type("Console.WriteLine", { delay: 1700 })
+  .delete(17)
+  .type("System.out.println", { delay: 1800 })
+  .delete(18)
   .go();
 })
